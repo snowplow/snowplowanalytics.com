f88cad3e4e0b9fb1ab36a0975de0fec157132322
Josh <joshua.a.beemster@gmail.com>
2017-03-23 16:58:12 +0100

    Stub repo with Vagrant and .travis configuration

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# snowplowanalytics.com
+|  **Branch**                | **Link**                                  | **Build Status**                                |
+|:--------------------------:|:-----------------------------------------:|:-----------------------------------------------:|
+|  [Master][branch-master]   | https://snowplowanalytics.com             | [![Build Status][travis-image-master]][travis]  |
+|  [Develop][branch-develop] | https://next.snowplowanalytics.com        | [![Build Status][travis-image-develop]][travis] |
+
+# What is *snowplowanalytics.com*?
+
+This repo contains the source code and content for the [Snowplow](https://snowplowanalytics.com) website. The site is built with [Jekyll](https://github.com/mojombo/jekyll) and published to Amazon S3 where it is then served by CloudFront.
+
+All content is Copyright © 2012-2017 Snowplow Analytics Ltd and not to be reused without permission.
+
+[branch-master]: https://github.com/snowplow/snowplowanalytics.com
+[branch-develop]: https://github.com/snowplow/snowplowanalytics.com/tree/develop
+
+[travis]: https://travis-ci.org/snowplow/snowplowanalytics.com
+[travis-image-master]: https://travis-ci.org/snowplow/snowplowanalytics.com.svg?branch=master
+[travis-image-develop]: https://travis-ci.org/snowplow/snowplowanalytics.com.svg?branch=develop
---
+++ vagrant/up.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+vagrant_dir=/vagrant/vagrant
+bashrc=/home/vagrant/.bashrc
+
+echo "========================================"
+echo "INSTALLING PERU AND ANSIBLE DEPENDENCIES"
+echo "----------------------------------------"
+apt-get update
+apt-get install -y language-pack-en git unzip libyaml-dev python3-pip python-pip python-yaml python-paramiko python-jinja2
+
+echo "==============="
+echo "INSTALLING PERU"
+echo "---------------"
+sudo pip3 install peru
+
+echo "=================="
+echo "INSTALLING ANSIBLE"
+echo "------------------"
+sudo pip install ansible=='1.8.4'
+
+echo "==================================="
+echo "CLONING ANSIBLE PLAYBOOKS WITH PERU"
+echo "-----------------------------------"
+cd ${vagrant_dir} && peru sync -v
+echo "... done"
+
+hosts=${vagrant_dir}/ansible.hosts
+ssh_checking=False
+
+echo "==================="
+echo "CONFIGURING ANSIBLE"
+echo "-------------------"
+touch ${bashrc}
+echo "export ANSIBLE_HOSTS=${hosts}" >> ${bashrc}
+echo "export ANSIBLE_HOST_KEY_CHECKING=${ssh_checking}" >> ${bashrc}
+echo "... done"
+
+echo "=========================================="
+echo "RUNNING PLAYBOOKS WITH ANSIBLE*"
+echo "* no output while each playbook is running"
+echo "------------------------------------------"
+while read pb; do
+    su - -c "ansible-playbook ${vagrant_dir}/${pb} --connection=local --inventory-file=${hosts}" vagrant
+done <${vagrant_dir}/up.playbooks
+
+guidance=${vagrant_dir}/up.guidance
+
+if [ -f ${guidance} ]; then
+    echo "==========="
+    echo "PLEASE READ"
+    echo "-----------"
+    cat $guidance
+fi
