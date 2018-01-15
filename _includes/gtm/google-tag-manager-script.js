a772443939da2bbaeadd7d474fcdb0a94a5fb9c1
Ben Fradet <benjamin.fradet@gmail.com>
2018-01-15 11:49:35 +0000

    Add Snowplow Google Analytics plugin

--- _includes/gtm/google-tag-manager-script.js
+++ _includes/gtm/google-tag-manager-script.js
@@ -5,3 +5,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PBMV98P');</script>
 <!-- End Google Tag Manager -->
+<script>
+window.ga = window.ga || function() { (window.ga.q = window.ga.q || []).push(arguments) }
+ga('create', 'UA-34290195-1', 'auto');
+ga('require', 'spGaPlugin', { endpoint: 'https://collector.snplow.com/' });
+ga('send', 'pageview');
+</script>
+<script async src="https://d1fc8wv8zag5ca.cloudfront.net/sp-ga-plugin/0.1.0-rc2/sp-ga-plugin.js"></script>
